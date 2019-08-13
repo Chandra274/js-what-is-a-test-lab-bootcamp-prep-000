7f6b272f7bdfe62ac8e851e595eddae806768fb2
Chandra Gopinathan <chandrag@globalcreditresearch.com>
2019-08-13 15:06:40 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,6 @@
 var index = require("../index.js")
 
-describe('Message', () => {
+describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
